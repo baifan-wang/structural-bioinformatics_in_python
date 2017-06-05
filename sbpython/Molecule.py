cc5fc04cf2eb00d3045e92cdce2326354c6cf7b6
Baifan Wang <ferlich1984@gmail.com>
2017-06-05 14:22:59 +0200

    add new function to 'Molecule.py'

--- sbpython/Molecule.py
+++ sbpython/Molecule.py
@@ -1,5 +1,10 @@
 import numpy as np
 class Molecule():
+    """
+    Create Molecule object.
+    The Molecule object stores Residue objects.
+    Residue objects are stored in a dict named residues.
+    """
     def __init__(self, residues=None, name=None, molecule_type=None):
         if residues is None:
             self.residues = {}
@@ -9,18 +14,37 @@ class Molecule():
         self.molecule_type = molecule_type #protien, DNA, RNA, HYB, small, ion
 
     def __getattr__(self, args):
+        """
+        Enable the access Atom and Residue in this Molecule object with the following syntax:
+        Molecule.chian+Residue_sreial.Atom_name, e.g.: M1.A12.CA, M2.B1.
+        """
         if args in self.residues:
             return self.residues[args]
         else:
             raise AttributeError('Not such residue in this molecule!')
 
     def __len__(self):
-        """Return the number of residues in this molecule."""
+        """
+        Return the number of residues in this molecule.
+        """
         return len(self.residues)
 
-    def __contains__(self, id):
-        """True if there is a residue in this molecule."""
-        return (id in self.residues)
+    def __contains__(self, residue):
+        """
+        True if there is a residue in this molecule.
+        """
+        id = residue.chain_id+residue.serial
+        return (residue.id in self.residues) and (residue.container == self)
+
+    def add_residue(self, residue):
+        """Adding a residue to this molecule.
+        raise KeyError if key conflict.
+        """
+        id = residue.chain_id+residue.serial
+        if id in self.residues:
+            raise KeyError("%s is already in this molecule!" %id)
+        self.residues[id] = residue
+        self.residues[id].container = self
 
     def get_atoms(self):
         for r in self.get_residues():
@@ -36,7 +60,9 @@ class Molecule():
         return r
 
     def coordinates(self):
-        """return the coordinates of all of the atoms"""
+        """
+        return the coordinates of all of the atoms
+        """
         c = []
         for r in self.residue_list():
             res = self.residues[r]
@@ -51,15 +77,21 @@ class Molecule():
         pass
 
     def center(self):
-        """return the average position of all atoms."""
+        """
+        return the average position of all atoms.
+        """
         pass
 
     def mass_center(self):
-        """return the center of mass of all atoms."""
+        """
+        return the center of mass of all atoms.
+        """
         pass
 
     def get_sequence(self):
-        """return the sequence of this molecule."""
+        """
+        return the sequence of this molecule.
+        """
         pass
 
     def backbone(self):
