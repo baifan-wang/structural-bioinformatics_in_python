95aa4224129a7d7413a9dc8f44306e653837cdf9
Baifan Wang <ferlich1984@gmail.com>
2017-07-05 10:38:27 +0200

    add a property, 'serial'

--- sbpython/__init__.py
+++ sbpython/__init__.py
@@ -16,7 +16,7 @@ from .Structural_alignment import *
 #from .atoms_to_model import *
 
 def atoms_to_model(atoms_list):
-    m = Model()
+    model = Model()
     i=0
     if len(atoms_list)==0:
         raise ValueError('No coordinates are found')
@@ -24,18 +24,26 @@ def atoms_to_model(atoms_list):
         m1 = Molecule()         # creat a Molecule object for each atom list in atoms_list
         i+=1                    # increasing model number by 1
         m1.name = 'm'+str(i)    # creat a name for each molecule
+        m1.serial = i
+        m = 1
+        n = 1
         r1 = Residue(atoms[0][4],atoms[0][2],atoms[0][3])  #create first residue from first atom.
+        r1.serial = m
         for a in atoms:         # for each line in atoms, which is a list
             a1 = Atom(a)        # create an Atom object form a list
+            a1.serial = n
             if a1.res_serial == r1.res_serial and a1.chain_id == r1.chain_id:
                 r1.add_atom(a1)
             else:
                 m1.add_residue(r1)
                 r1 = Residue(a[4], a[2], a[3])
+                m+=1
+                r1.serial = m
                 r1.add_atom(a1)
+            n+=1
         m1.add_residue(r1)
-        m.add_molecule(m1)
-    return m
+        model.add_molecule(m1)
+    return model
 
 def get_model(file):
     if not os.path.exists(file):
