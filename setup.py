bedc600d11aea93b51e0261527ae2a80dc90f1f4
Baifan Wang <ferlich1984@gmail.com>
2017-08-02 16:53:02 +0200

    add 'setup.py'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+import sys
+
+
+
+setup(
+    name="SBio",
+    url='https://github.com/baifan-wang/structural-bioinformatics_in_python',
+    description='structural bioinformatics in python',
+    packages=find_packages(),
+    zip_safe=False)
